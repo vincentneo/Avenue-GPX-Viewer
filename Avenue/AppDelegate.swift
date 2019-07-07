db5366cd552e2af5138217f8669be9c6c64c81d7
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-07-07 18:07:13 +0800

    Initial Commit

---
+++ Avenue/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Avenue
+//
+//  Created by Vincent on 7/7/19.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Avenue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Avenue
+//
+//  Created by Vincent on 7/7/19.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
