aeef10a3c5230d500c6282ebf25183bdca65a935
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-03-10 17:10:00 +0800

    fix bug that may cause QuickLook extension to crash

--- Avenue/ElapsedTime.swift
+++ Avenue/ElapsedTime.swift
@@ -19,32 +19,21 @@ class ElapsedTime {
     ///    2. 3h 40 min 30 sec, returns  `3 h 40 min 20 s`
     ///
     static func getString(from elapsedTime: TimeInterval) -> String {
-       var tmpTime: TimeInterval = elapsedTime
-       //calculate the minutes and hours in elapsed time.
-
-       let hours = UInt32(tmpTime / 3600.0)
-       tmpTime -= (TimeInterval(hours) * 3600)
-
-       let minutes = UInt32(tmpTime / 60.0)
-       tmpTime -= (TimeInterval(minutes) * 60)
-
-       //calculate the seconds in elapsed time.
-       let seconds = UInt32(tmpTime)
-       tmpTime -= TimeInterval(seconds)
-
-       //display hours only if >0
-       let strHours = hours > 0 ? String(hours) + "h " : ""
-       //add the leading zero for minutes, seconds and millseconds and store them as string constants
-
-       let strMinutes = String(minutes)
-       let strSeconds = String(seconds)
-
-       //concatenate hours, minutes and seconds
-       return "\(strHours)\(strMinutes)min \(strSeconds)s"
+        if elapsedTime < 0 {
+            return "0min 0s"
+        }
+        let hours = Int(elapsedTime) / 3600
+        let minutes = (Int(elapsedTime) / 60) % 60
+        let seconds = Int(elapsedTime) % 60
+        
+        //display hours only if >0
+        let strHours = hours > 0 ? String(hours) + "h " : ""
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(minutes)
+        let strSeconds = String(seconds)
+        
+        //concatenate hours, minutes and seconds
+        return "\(strHours)\(strMinutes)min \(strSeconds)s"
     }
-    
-    /// Calls the delegate (didUpdateElapsedTimeString) to inform there was an update of the elapsed time.
-    //@objc func updateElapsedTime() {
-   //     self.delegate?.stopWatch(self, didUpdateElapsedTimeString: self.elapsedTimeString)
-   // }
 }
