14879d0297fe3b14343084b819cacd3819f20314
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2023-01-13 20:58:25 +0800

    route quicklook support

--- GPXQuickLook/QLMapView.swift
+++ GPXQuickLook/QLMapView.swift
@@ -15,16 +15,12 @@ class QLMapView: MKMapView {
     var timeInterval = 0.0
     
     func loadedGPXFile(_ root: GPXRoot) {
-        print("MapView: GPX Object Loaded \(root)")
-        for track in root.tracks {
-            for trackseg in track.segments {
-                length += trackseg.length()
-            }
-            for trackseg in track.segments {
-                guard let startTime = trackseg.points.first?.time,
-                      let endTime = trackseg.points.last?.time else { continue }
-                let timeBetween = endTime.timeIntervalSince(startTime)
-                timeInterval += timeBetween
+        print("MapView: GPX Object Loaded \(root)")        
+        for route in root.routes {
+            let overlay = route.overlay
+            self.addOverlay(overlay, level: .aboveLabels)
+            for point in route.points {
+                self.extent.extendAreaToIncludeLocation(point.coordinate)
             }
         }
         
@@ -35,12 +31,18 @@ class QLMapView: MKMapView {
         for track in root.tracks {
             for segment in track.segments {
                 let overlay = segment.overlay
+                length += segment.length()
 
                 self.addOverlay(overlay, level: .aboveLabels)
                 
                 for trkpt in segment.points {
                     self.extent.extendAreaToIncludeLocation(trkpt.coordinate)
                 }
+                
+                guard let startTime = segment.points.first?.time,
+                      let endTime = segment.points.last?.time else { continue }
+                let timeBetween = endTime.timeIntervalSince(startTime)
+                timeInterval += timeBetween
             }
         }
         // reduce region span being too close to bounds of view
