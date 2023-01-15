e77613b06cd49cc71d64bd7dad0bdf0b4b5a50f2
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2023-01-15 12:23:56 +0800

    distance filtering, drawing changes

--- Avenue/AvenuePolylineRenderer.swift
+++ Avenue/AvenuePolylineRenderer.swift
@@ -14,36 +14,55 @@ class AvenuePolylineRenderer: MKPolylineRenderer {
 
     override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
         super.draw(mapRect, zoomScale: zoomScale, in: context)
-        let roadWidth = MKRoadWidthAtZoomScale(zoomScale) * 3
+        let roadWidth = MKRoadWidthAtZoomScale(zoomScale) * 2
         
         var drawRect = NSRect(origin: .zero, size: CGSize(width: roadWidth, height: roadWidth))
         guard let cgImage = image.cgImage(forProposedRect: &drawRect, context: .current, hints: nil) else { return }
         
         let mapPoints = self.polyline.points()
         
-        if zoomScale >= 0.2 {
-            for idx in 0..<self.polyline.pointCount - 2 {
-                guard mapRect.contains(mapPoints[idx]) || mapRect.contains(mapPoints[idx + 1]) else { continue }
-                let startPoint = self.point(for: mapPoints[idx])
-                let endPoint = self.point(for: mapPoints[idx + 1])
-                
-                //let midPoint = CGPoint(x: (startPoint.x + endPoint.x) / 2, y: (startPoint.y + endPoint.y) / 2)
-                let bearing =  (atan2(startPoint.y - endPoint.y, startPoint.x - endPoint.x) - .pi)
-                
-                let affirm = CGAffineTransform(scaleX: 1, y: -1)
-                let adjustedBearing = -((2.5 * .pi) + bearing)
-                let rotate = affirm.rotated(by: adjustedBearing)
-                let transformed = startPoint.applying(rotate)
-                let newPoint = CGPoint(x: transformed.x - drawRect.midX, y: transformed.y - drawRect.midY)
-                context.saveGState()
-                context.scaleBy(x: 1, y: -1)
-                context.rotate(by: adjustedBearing)
-                context.setBlendMode(.luminosity)
-                context.draw(cgImage, in: NSRect(origin: newPoint, size: drawRect.size))
-                context.restoreGState()
-                
+        var prevMapPoint: MKMapPoint?
+        
+        for idx in 0..<self.polyline.pointCount - 2 {
+            guard mapRect.contains(mapPoints[idx]) || mapRect.contains(mapPoints[idx + 1]) else { continue }
+            let startMapPoint = mapPoints[idx]
+            let endMapPoint = mapPoints[idx + 1]
+            let startPoint = self.point(for: startMapPoint)
+            let endPoint = self.point(for: endMapPoint)
+            let region = MKCoordinateRegion(mapRect)
+            let latDist = region.span.latitudeDelta * 110574 // https://ada-lovecraft.github.io/post/geopositional-deltas-expressed-as-meters/
+            let distanceFilter = 0.12 * latDist
+            
+            if let previous = prevMapPoint {
+                if previous.distance(to: endMapPoint) > distanceFilter {
+                    prevMapPoint = nil
+                }
+                else {
+                    continue
+                }
             }
+            else if startMapPoint.distance(to: endMapPoint) < distanceFilter {
+                prevMapPoint = startMapPoint
+                continue
+            }
+
+            //let midPoint = CGPoint(x: (startPoint.x + endPoint.x) / 2, y: (startPoint.y + endPoint.y) / 2)
+            let bearing = atan2(startPoint.y - endPoint.y, startPoint.x - endPoint.x) - .pi
+            let scaleTransform = CGAffineTransform(scaleX: 1, y: -1)
+            let adjustedBearing = -((2.5 * .pi) + bearing)
+            let rotateTransform = scaleTransform.rotated(by: adjustedBearing)
+            let transformed = startPoint.applying(rotateTransform)
+            let newPoint = CGPoint(x: transformed.x - drawRect.midX, y: transformed.y - drawRect.midY)
+            context.saveGState()
+            context.scaleBy(x: 1, y: -1)
+            context.rotate(by: adjustedBearing)
+            context.setBlendMode(.luminosity)
+            context.setAlpha(0.85)
+            context.draw(cgImage, in: NSRect(origin: newPoint, size: drawRect.size))
+            context.restoreGState()
+            
         }
+        
         
     }
 
